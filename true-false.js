23522eba51c669b71ce384fee4fb491abadd1801
ARASHFAQUE <55973326+ARASHFAQUE@users.noreply.github.com>
2020-08-07 17:44:57 +0600

    another-commit

--- true-false.js
+++ true-false.js
@@ -1,4 +1,4 @@
-//False ==> 0, Empty String(""), undefine(var name;), null(var name = null;), Not a Number(NaN(var name = NaN)), false .
+//False ==> 0, Empty String(""), undefined(var name;), null(var name = null;), Not a Number(NaN(var name = NaN)), false .
 //True ==> Number Without Zero(0), Zero Inside Quotation('0'),Space Inside Quotation(' '), Empty Array [], Empty Object{}, 
 // let name = NaN;
 let name = 0;
