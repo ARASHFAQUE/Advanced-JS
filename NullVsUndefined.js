da60892fd7cde85e12948d0c20aee35e92001ba4
ARASHFAQUE <55973326+ARASHFAQUE@users.noreply.github.com>
2020-08-07 18:00:20 +0600

    null vs undefined cleared

---
+++ NullVsUndefined.js
@@ -0,0 +1,29 @@
+// Undefined ==>
+
+let pakhi;
+console.log(pakhi);
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return;
+}
+const result = add(5,6);
+console.log(result);
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+const result = add(5);
+console.log(result);
+
+const premik = {name: 'Smart Dude', phone: 458421};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,9];
+console.log(ages[11]);
+
+//Null ==>
+
